bf3160470110d0837da22b86cc0a905ce98dfe4b
Wayne Landini <wlandini@mines.edu>
2012-08-30 20:56:58 -0600

    APT 2 finished.

---
+++ LunchList/src/csci498/wlandini/lunchlist/Restaurant.java
@@ -0,0 +1,23 @@
+package csci498.wlandini.lunchlist;
+
+public class Restaurant {
+	private String name = "";
+	private String address = "";
+	
+	public String getName(){
+		return(name);
+	}
+	
+	public void setName(String Name){
+		this.name = name;
+	}
+	
+	public String getAddress(){
+		return(address);
+	}
+	
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+}
