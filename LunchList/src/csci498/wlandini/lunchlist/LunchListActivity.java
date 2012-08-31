56494c06cc177e74bfd58c83408236964f814fd8
Wayne Landini <wlandini@mines.edu>
2012-08-31 00:08:33 -0600

    APT #3 done with extra credit attempted.

--- LunchList/src/csci498/wlandini/lunchlist/LunchListActivity.java
+++ LunchList/src/csci498/wlandini/lunchlist/LunchListActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class LunchListActivity extends Activity {
     Restaurant r = new Restaurant();
@@ -15,9 +18,7 @@ public class LunchListActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
         Button save = (Button)findViewById(R.id.save);
-        
         save.setOnClickListener(onSave);
     }
     
@@ -29,6 +30,17 @@ public class LunchListActivity extends Activity {
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
 			RadioGroup types = (RadioGroup)findViewById(R.id.types);
+			//This is how you create the radio buttons without using the xml file
+//			RadioButton rb1 = new RadioButton(null);
+//			RadioButton rb2 = new RadioButton(null);
+//			RadioButton rb3 = new RadioButton(null);
+//			rb1.setText("Take-Out");
+//			rb2.setText("Sit-Down");
+//			rb3.setText("Delivery");
+//			types.addView(rb1);
+//			types.addView(rb2);
+//			types.addView(rb3);
+			
 			switch(types.getCheckedRadioButtonId()){
 				case R.id.sit_down:
 					r.setType("sit_down");
