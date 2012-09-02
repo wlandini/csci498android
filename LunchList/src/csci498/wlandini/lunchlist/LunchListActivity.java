68b95894cf31e0bd2bc028ccfb5ede220654e9e3
Wayne Landini <wlandini@mines.edu>
2012-09-02 00:44:48 -0600

    Address editText changed to an AutoCompleteTextView

--- LunchList/src/csci498/wlandini/lunchlist/LunchListActivity.java
+++ LunchList/src/csci498/wlandini/lunchlist/LunchListActivity.java
@@ -19,8 +19,10 @@ import android.widget.TextView;
 
 public class LunchListActivity extends Activity {
     List<Restaurant> model = new ArrayList<Restaurant>();
+    List<String> addresses = new ArrayList<String>();
+    AutoCompleteTextView address;
 	ArrayAdapter<Restaurant> adapter = null;
-	SpinnerAdapter adapter2 = null;
+	ArrayAdapter<String> adapter2 = null;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,10 +31,13 @@ public class LunchListActivity extends Activity {
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
         ListView list = (ListView)findViewById(R.id.restaurants);
+        AutoCompleteTextView address = (AutoCompleteTextView)findViewById(R.id.addr);
         //Spinner s = (Spinner)findViewById(R.id.restaurants);
         adapter = new ArrayAdapter<Restaurant>(this,android.R.layout.simple_list_item_1,model);
+        adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,addresses);
         //adapter = new ArrayAdapter<Restaurant>(this,android.R.layout.simple_spinner_dropdown_item,model);
         list.setAdapter(adapter);
+        address.setAdapter(adapter2);
         //s.setAdapter(adapter);
     }
     
@@ -40,10 +45,8 @@ public class LunchListActivity extends Activity {
 		public void onClick(View v) {
 			Restaurant r = new Restaurant();
 			EditText name = (EditText)findViewById(R.id.name);
-			EditText address = (EditText)findViewById(R.id.addr);
-			//AutoCompleteTextView address = (AutoCompleteTextView)findViewById(R.id.addr);
-			//address.setAdapter(adapter);
 			//EditText address = (EditText)findViewById(R.id.addr);
+			AutoCompleteTextView address = (AutoCompleteTextView)findViewById(R.id.addr);
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
 			RadioGroup types = (RadioGroup)findViewById(R.id.types);
@@ -72,6 +75,7 @@ public class LunchListActivity extends Activity {
 					break;
 			}
 			adapter.add(r);
+			adapter2.add(r.getAddress());
 		}
 	};
 }
