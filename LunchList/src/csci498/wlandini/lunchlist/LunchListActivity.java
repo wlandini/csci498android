a6e4d7502b983992c939ca3a698867e73bc03418
Wayne Landini <wlandini@mines.edu>
2012-09-26 14:18:43 -0600

    Preference actitivty connected to the options menu

--- LunchList/src/csci498/wlandini/lunchlist/LunchListActivity.java
+++ LunchList/src/csci498/wlandini/lunchlist/LunchListActivity.java
@@ -49,6 +49,10 @@ public class LunchListActivity extends ListActivity {
 		  startActivity(new Intent(LunchListActivity.this, DetailForm.class));
 		  return(true);
 	  }
+	  else if (item.getItemId() == R.id.prefs){
+		  startActivity(new Intent(this, EditPreferences.class));
+		  return(true);
+	  }
 	  return(super.onOptionsItemSelected(item));
   }
   
