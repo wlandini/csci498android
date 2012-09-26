4b9d8af58b66610ec32199cb3816cc03f8d613ed
Wayne Landini <wlandini@mines.edu>
2012-09-26 14:07:54 -0600

    Preference activity created

---
+++ LunchList/src/csci498/wlandini/lunchlist/EditPreferences.java
@@ -0,0 +1,12 @@
+package csci498.wlandini.lunchlist;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class EditPreferences extends PreferenceActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
