d92c57dea08ce44f7c420b3480be8fba0d57194b
Wayne Landini <wlandini@mines.edu>
2012-09-27 11:09:31 -0600

    Adding missed files

---
+++ LunchList/src/csci498/wlandini/lunchlist/DetailForm.java
@@ -0,0 +1,85 @@
+package csci498.wlandini.lunchlist;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class DetailForm extends Activity {
+	EditText name = null;
+	EditText address = null;
+	EditText notes = null;
+	RadioGroup types = null;
+	RestaurantHelper helper = null;
+	String restaurantId = null;
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail_form);
+		
+		helper = new RestaurantHelper(this);
+		name = (EditText)findViewById(R.id.name);
+		address = (EditText)findViewById(R.id.addr);
+		notes = (EditText)findViewById(R.id.notes);
+		types = (RadioGroup)findViewById(R.id.types);
+		Button save = (Button)findViewById(R.id.save);
+		save.setOnClickListener(onSave);
+		if(restaurantId != null){
+			load();
+		}
+	}
+	private void load(){
+		Cursor c = helper.getById(restaurantId);
+		c.moveToFirst();
+		name.setText(helper.getName(c));
+		address.setText(helper.getAddress(c));
+		notes.setText(helper.getNotes(c));
+		
+		if(helper.getType(c).equals("sit_down")){
+			types.check(R.id.sit_down);
+		}
+		else if (helper.getType(c).equals("take_out")){
+			types.check(R.id.take_out);
+		}
+		else{
+			types.check(R.id.delivery);
+		}
+		c.close();
+	}
+	private View.OnClickListener onSave = new View.OnClickListener() {
+		
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			String type = null;
+			
+			switch(types.getCheckedRadioButtonId()){
+				case R.id.sit_down:
+					type = "sit_down";
+					break;
+				case R.id.take_out:
+					type = "take_out";
+					break;
+				case R.id.delivery:
+					type = "delivery";
+					break;
+			}
+			if(restaurantId == null){
+				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				
+			}
+			else{
+				helper.update(restaurantId,name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+			}
+			finish();
+		}
+	};
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		helper.close();
+	}
+}
