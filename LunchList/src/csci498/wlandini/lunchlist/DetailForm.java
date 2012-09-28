349b37a80d5085637611ddbe34020f4686905650
Wayne Landini <wlandini@mines.edu>
2012-09-28 15:04:40 -0600

    Adding contents to onSaveInstanceState()

--- LunchList/src/csci498/wlandini/lunchlist/DetailForm.java
+++ LunchList/src/csci498/wlandini/lunchlist/DetailForm.java
@@ -20,6 +20,10 @@ public class DetailForm extends Activity {
 	@Override
 	public void onSaveInstanceState(Bundle state) {
 		super.onSaveInstanceState(state);
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes", notes.getText().toString());
+		state.putInt("type", types.getCheckedRadioButtonId());
 	}
 	
 	@Override
