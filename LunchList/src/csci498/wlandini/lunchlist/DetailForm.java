bcb9d23fad71b4e7a08ac494365f5f492117bf64
Wayne Landini <wlandini@mines.edu>
2012-09-29 21:05:50 -0600

    Fetching the feed

--- LunchList/src/csci498/wlandini/lunchlist/DetailForm.java
+++ LunchList/src/csci498/wlandini/lunchlist/DetailForm.java
@@ -1,12 +1,14 @@
 package csci498.wlandini.lunchlist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
