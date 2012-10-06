0847fa23069fd85eebe8575ac00dc217f944ed0f
Wayne Landini <wlandini@mines.edu>
2012-10-06 15:36:52 -0600

    Moving feed fetching and parsing to the service

--- LunchList/src/csci498/wlandini/lunchlist/FeedService.java
+++ LunchList/src/csci498/wlandini/lunchlist/FeedService.java
@@ -1,15 +1,26 @@
 package csci498.wlandini.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	public static final String EXTRA_URL = "csci498.wlandini.lunchlist.EXTRA_URL";
 	public FeedService() {
 		super("FeedService");
 	}
 	
 	@Override
 	public void onHandleIntent(Intent i) {
-		
+		RSSReader reader = new RSSReader();
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		}
+		catch (Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 }
