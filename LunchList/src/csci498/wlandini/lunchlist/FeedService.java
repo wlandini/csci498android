40182beffa692bdcb71c3d89f402d925c3e796b3
Wayne Landini <wlandini@mines.edu>
2012-10-06 15:44:26 -0600

    Sending the feed to the activity

--- LunchList/src/csci498/wlandini/lunchlist/FeedService.java
+++ LunchList/src/csci498/wlandini/lunchlist/FeedService.java
@@ -3,12 +3,18 @@ package csci498.wlandini.lunchlist;
 import org.mcsoxford.rss.RSSFeed;
 import org.mcsoxford.rss.RSSReader;
 
+import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
 
 public class FeedService extends IntentService {
+	
 	public static final String EXTRA_URL = "csci498.wlandini.lunchlist.EXTRA_URL";
+	public static final String EXTRA_MESSENGER = "csci498.wlandini.lunchlist.EXTRA_MESSENGER";
+	
 	public FeedService() {
 		super("FeedService");
 	}
@@ -16,11 +22,25 @@ public class FeedService extends IntentService {
 	@Override
 	public void onHandleIntent(Intent i) {
 		RSSReader reader = new RSSReader();
+		Messenger messenger = (Messenger)i.getExtras().get(EXTRA_MESSENGER);
+		Message msg = Message.obtain();
+		
 		try {
 			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+			msg.arg1 = Activity.RESULT_OK;
+			msg.obj = result;
 		}
 		catch (Exception e) {
 			Log.e("LunchList", "Exception parsing feed", e);
+			msg.arg1 = Activity.RESULT_CANCELED;
+			msg.obj = e;
+		}
+		
+		try {
+			messenger.send(msg);
+		}
+		catch (Exception e) {
+			Log.w("LunchList", "Exception sending results to activity", e);
 		}
 	}
 }
