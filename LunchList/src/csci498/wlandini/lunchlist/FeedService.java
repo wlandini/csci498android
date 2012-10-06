05a1102a9918668f0564d234512a9943240272df
Wayne Landini <wlandini@mines.edu>
2012-10-06 15:33:15 -0600

    Creating and registering a Stub IntentService

---
+++ LunchList/src/csci498/wlandini/lunchlist/FeedService.java
@@ -0,0 +1,15 @@
+package csci498.wlandini.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+	public FeedService() {
+		super("FeedService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent i) {
+		
+	}
+}
