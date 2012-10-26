74275d35216a4f994ce440b578a0c5d50dfba243
Wayne Landini <wlandini@mines.edu>
2012-10-26 09:16:46 -0600

    Setting up a Boot-Time Reciever

---
+++ LunchList/src/OnBootReceiver.java
@@ -0,0 +1,14 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context ctxt, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
