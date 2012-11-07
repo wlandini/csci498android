31837d46a556ed980e09f7886b914a28dc1f181b
Wayne Landini <wlandini@mines.edu>
2012-11-06 19:31:46 -0700

    Implementing the RemoteViewsFactory

--- LunchList/src/csci498/wlandini/lunchlist/ListWidgetService.java
+++ LunchList/src/csci498/wlandini/lunchlist/ListWidgetService.java
@@ -4,10 +4,8 @@ import android.content.Intent;
 import android.widget.RemoteViewsService;
 
 public class ListWidgetService extends RemoteViewsService {
-
-	@Override
-	public RemoteViewsFactory onGetViewFactory(Intent intent) {
-		return new ListViewsFactory(this.getApplicationContext(), intent);
-	}
-
+  @Override
+  public RemoteViewsFactory onGetViewFactory(Intent intent) {
+	  return new ListViewsFactory(this.getApplicationContext(), intent);
+  }
 }
