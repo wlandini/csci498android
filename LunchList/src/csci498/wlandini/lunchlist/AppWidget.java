8cda009fd3f6ed777c6da88e708f895dfbba678d
Wayne Landini <wlandini@mines.edu>
2012-11-06 18:03:57 -0700

    Detecting which app widget to display

--- LunchList/src/csci498/wlandini/lunchlist/AppWidget.java
+++ LunchList/src/csci498/wlandini/lunchlist/AppWidget.java
@@ -4,10 +4,16 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class AppWidget extends AppWidgetProvider {
 	@Override
 	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			onHCUpdate(ctxt, mgr, appWidgetIds);
+		} else {
+			ctxt.startService(new Intent(ctxt, WidgetService.class));
+		}
 		ctxt.startService(new Intent(ctxt, WidgetService.class));
 	}
 }
