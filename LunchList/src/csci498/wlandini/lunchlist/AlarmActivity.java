559794b8afe2787a327749f34c40e86aeb59d94c
Wayne Landini <wlandini@mines.edu>
2012-11-19 20:22:40 -0700

    Playing the ringtone with the Activity

--- LunchList/src/csci498/wlandini/lunchlist/AlarmActivity.java
+++ LunchList/src/csci498/wlandini/lunchlist/AlarmActivity.java
@@ -1,12 +1,47 @@
 package csci498.wlandini.lunchlist;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 
-public class AlarmActivity extends Activity {
+public class AlarmActivity extends Activity
+	implements MediaPlayer.OnPreparedListener {
+	MediaPlayer player = new MediaPlayer();
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.alarm);
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		String sound = prefs.getString("alarm_ringtone", null);
+		
+		if (sound != null) {
+			player.setAudioStreamType(AudioManager.STREAM_ALARM);
+			
+			try {
+				player.setDataSource(sound);
+				player.setOnPreparedListener(this);
+				player.prepareAsync();
+			} catch (Exception e) {
+				Log.e("LunchList", "Exception in playing ringtone", e);
+			}
+		}
+	}
+	
+	@Override
+	public void onPause() {
+		if (player.isPlaying()) {
+			player.stop();
+		}
+		
+		super.onPause();
+	}
+	
+	public void onPrepared(MediaPlayer player) {
+		player.start();
 	}
 }
