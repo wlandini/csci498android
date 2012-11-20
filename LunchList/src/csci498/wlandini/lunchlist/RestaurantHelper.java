d28d34c91ff7b212be54086a1b41b39a5db44727
Wayne Landini <wlandini@mines.edu>
2012-11-19 20:58:15 -0700

    Adding Phone Number Support to the Rest of the Helper

--- LunchList/src/csci498/wlandini/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/wlandini/lunchlist/RestaurantHelper.java
@@ -45,25 +45,26 @@ class RestaurantHelper extends SQLiteOpenHelper {
   }
 
   public Cursor getAll(String orderBy) {
-    return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, lat, lon  FROM restaurants ORDER BY " + orderBy, null);
+    return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, lat, lon, phone  FROM restaurants ORDER BY " + orderBy, null);
   }
   
   public Cursor getById(String id) {
     String[] args = {id};
-    return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants WHERE _ID=?",args);
+    return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon, phone FROM restaurants WHERE _ID=?",args);
   }
   
-  public void insert(String name, String address, String type, String notes, String feed) {
+  public void insert(String name, String address, String type, String notes, String feed, String phone) {
     ContentValues cv = new ContentValues();
     cv.put("name", name);
     cv.put("address", address);
     cv.put("type", type);
     cv.put("notes", notes);
     cv.put("feed", feed);
+    cv.put("phone", phone);
     getWritableDatabase().insert("restaurants", "name", cv);
   }
   
-  public void update(String id, String name, String address, String type, String notes, String feed) {
+  public void update(String id, String name, String address, String type, String notes, String feed, String phone) {
     ContentValues cv = new ContentValues();
     String[] args = {id};
     cv.put("name", name);
@@ -71,6 +72,7 @@ class RestaurantHelper extends SQLiteOpenHelper {
     cv.put("type", type);
     cv.put("notes", notes);
     cv.put("feed", feed);
+    cv.put("phone", phone);
     getWritableDatabase().update("restaurants", cv, "_ID=?", args);
   }
   
@@ -100,6 +102,10 @@ class RestaurantHelper extends SQLiteOpenHelper {
   
   public double getLongitude(Cursor c) {
 	  return c.getDouble(7);
+  }
+  
+  public String getPhone(Cursor c) {
+	  return c.getString(8);
   }
   
 }
