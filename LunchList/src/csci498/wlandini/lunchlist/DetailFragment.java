52d0783b3cddcd1840a20bd978a04e6fb1b81f92
Wayne Landini <wlandini@mines.edu>
2012-11-19 21:29:09 -0700

    Dialing the number.

--- LunchList/src/csci498/wlandini/lunchlist/DetailFragment.java
+++ LunchList/src/csci498/wlandini/lunchlist/DetailFragment.java
@@ -8,6 +8,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -114,6 +115,10 @@ public class DetailFragment extends Fragment {
       menu.findItem(R.id.location).setEnabled(false);
       menu.findItem(R.id.map).setEnabled(false);
     }
+    
+    if (isTelephonyAvailable()) {
+    	menu.findItem(R.id.call).setEnabled(true);
+    }
   }
   
   @Override
@@ -138,6 +143,12 @@ public class DetailFragment extends Fragment {
       i.putExtra(RestaurantMap.EXTRA_NAME, name.getText().toString());
       startActivity(i);
       return true;
+    } else if (item.getItemId() == R.id.call) {
+    	String toDial = "tel:" + phone.getText().toString();
+    	
+    	if (toDial.length() > 4) {
+    		startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(toDial)));
+    	}
     } else if (item.getItemId() == R.id.help) {
     	startActivity(new Intent(getActivity(), HelpPage.class));
     }
